26852f3ac59e3ba8e54beda82d67374a1b95589b
Mannan Alisco <amannan2132@gmail.com>
2024-06-19 16:04:47 +0600

    Add tests for placeShip function

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -228,3 +228,34 @@ describe("Check placeShipVertically", () => {
     expect(board.ships.length).toBe(3);
   });
 });
+
+describe("Check placeShip", () => {
+  test("placeShip", () => {
+    const board = new Gameboard();
+
+    board.placeShip(5, "1,6", "vertical");
+    board.placeShip(4, "8,1", "horizontal");
+    board.placeShip(3, "5,1", "horizontal");
+    board.placeShip(2, "2,1", "horizontal");
+    board.placeShip(2, "7,9", "vertical");
+    board.placeShip(3, "3,5", "horizontal"); //<<< will not place due to collision with ship being present
+    board.placeShip(4, "3,8", "horizontal"); //<<< will not place due to getting out of board
+
+    expect(board.ships.length).toBe(5);
+
+    expect(board.board[1][6].occupier).toBe(0);
+    expect(board.board[5][6].occupier).toBe(0);
+
+    expect(board.board[8][1].occupier).toBe(1);
+    expect(board.board[8][4].occupier).toBe(1);
+
+    expect(board.board[5][1].occupier).toBe(2);
+    expect(board.board[5][3].occupier).toBe(2);
+
+    expect(board.board[2][1].occupier).toBe(3);
+    expect(board.board[2][2].occupier).toBe(3);
+
+    expect(board.board[7][9].occupier).toBe(4);
+    expect(board.board[8][9].occupier).toBe(4);
+  });
+});
