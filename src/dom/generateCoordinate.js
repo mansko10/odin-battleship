4fad940cfbc77a948e88f4eb55fe29a539761609
Mannan Alisco <amannan2132@gmail.com>
2024-06-29 11:41:37 +0600

    Make computer moves smarter
    Ensure that adjacent cells of a sunk ship won't be hit

--- src/dom/generateCoordinate.js
+++ src/dom/generateCoordinate.js
@@ -1,7 +1,7 @@
 import genAdjacent from "./genAdjacent.js";
 
 let adjacents = [];
-let ship = [];
+let lastOccupied = [];
 
 export default function generateCoordinate(player) {
   if (adjacents.length > 0) {
@@ -14,9 +14,17 @@ export default function generateCoordinate(player) {
     //prettier-ignore
     if (player.gameboard.board[lastAdjacentSplitRow][lastAdjacentSplitColumn].isOccupied) {
       //Handles case where adjacent itself is occupied. The adjacent's adjacents will also be concatenated after adjacent is popped
+      const targettedShipIdentifier = player.gameboard.board[lastAdjacentSplitRow][lastAdjacentSplitColumn].occupier;
+      const shipInBoard = player.gameboard.ships[targettedShipIdentifier];
+      
+      lastOccupied.push(`${lastAdjacentSplitRow},${lastAdjacentSplitColumn}`);
+      console.log(lastOccupied);
       let additionalAdjacent = genAdjacent(lastAdjacentSplitRow, lastAdjacentSplitColumn, player);
       adjacents.pop();
       adjacents = adjacents.concat(additionalAdjacent);
+      
+      //IsSunk won't work (due to return being later) so came up with this way. The goal is to prevent hitting adjacent cells of already sunk ship.
+      if (shipInBoard.length - shipInBoard.timesHit === 1) adjacents = [];
       console.log(adjacents);
       return lastAdjacent;
     } else {
@@ -39,6 +47,7 @@ export default function generateCoordinate(player) {
     }
 
     if (player.gameboard.board[row][column].isOccupied) {
+      lastOccupied.push(`${row},${column}`);
       //Checks if coordinates randomly generated is occupied. If it is, then its adjacents are concatenated to adjacents array
       let additionalAdjacent = genAdjacent(row, column, player);
 
