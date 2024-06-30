8eb7f21f218c180d9a3d5c4eaf2492507b5ac050
Mannan Alisco <amannan2132@gmail.com>
2024-06-30 21:31:54 +0600

    Implement victory/defeat sounds

--- src/dom/endGame.js
+++ src/dom/endGame.js
@@ -1,5 +1,17 @@
+import victory from "../sounds/victory.mp3";
+import defeat from "../sounds/defeat.mp3";
+
 export default function endGame(player) {
   const body = document.querySelector("body");
+
+  const victorySound = new Audio(victory);
+  const defeatSound = new Audio(defeat);
+
+  if (player.name !== "computer") {
+    victorySound.play();
+  } else {
+    defeatSound.play();
+  }
 
   body.innerHTML = `<h1>${player.name[0].toUpperCase() + player.name.slice(1)} won!</h1>`;
 }
