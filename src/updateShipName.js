e02efeaa2476d237d179f7c5201642581f13a711
Mannan Alisco <amannan2132@gmail.com>
2024-06-30 18:00:46 +0600

    Create the updateShipName function
    This function will update the name of one ship to be placed after
    another one has been placed

---
+++ src/updateShipName.js
@@ -0,0 +1,8 @@
+export default function updateShipPlaceName(obj) {
+  if (!obj.length) return;
+  const dialog = document.querySelector("dialog");
+  const span = dialog.querySelector("#shipName");
+
+  span.style.textDecoration = "underline";
+  span.textContent = obj[0].name;
+}
