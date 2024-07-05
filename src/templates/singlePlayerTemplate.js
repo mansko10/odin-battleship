058705b7acdecb3dbf44caa321774cff101b7fa9
Mannan Alisco <amannan2132@gmail.com>
2024-07-05 19:04:22 +0600

    Reorganize singlePlayerTemplate

--- src/templates/singlePlayerTemplate.js
+++ src/templates/singlePlayerTemplate.js
@@ -1,13 +1,18 @@
 export default function generateSinglePlayerTemplate(name) {
-  const body = document.querySelector("body");
-  body.innerHTML += `<div class="boards">
-        <div class="player">
-          <h3>${name}</h3>
-          <div class="player board"></div>
-        </div>
-        <div class="computer">
-          <h3>Computer</h3>
-          <div class="computer board"></div>
-        </div>
-      </div>`;
+  const turnsDiv = document.querySelector(".turns");
+  const boardsDiv = document.createElement("div");
+
+  const template = `<div class="playerDisplay">
+            <h3>${name}</h3>
+            <div class="player board"></div>
+          </div>
+          <div class="computer">
+            <h3>Computer</h3>
+            <div class="computer board"></div>
+          </div>`;
+
+  boardsDiv.classList.add("boards");
+  boardsDiv.innerHTML = template;
+
+  turnsDiv.insertAdjacentElement("afterend", boardsDiv);
 }
